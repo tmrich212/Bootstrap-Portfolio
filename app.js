c3d3688016a19ed002047e381fc65b87993546cb
tmrich212 <57809945+tmrich212@users.noreply.github.com>
2021-11-28 19:54:23 -0500

    js added

---
+++ app.js
@@ -0,0 +1,50 @@
+let typeText = document.querySelector('.typeText');
+let textToType = "Front-end Developer from Virginia";
+let index = 0, isAdding = true;
+
+function playAnim() {
+  setTimeout(function (){
+    // set text of typeText to textToType using index
+    typeText.innerText = textToType.slice(0, index)
+    if (isAdding) {
+      // adds text
+      if(index > textToType.length){
+        // stops adding text
+        isAdding = false;
+        // break: wait 2s before playing again
+        setTimeout( function () {
+          playAnim()
+        }, 1800)
+        return;
+      } else {
+        // increment index by 1
+        index++;
+      }
+    } else {
+      // remove text
+      if(index === 0) {
+        // no more text to remove
+        isAdding = true
+      } else {
+        // decrement index by 1
+        index--;
+      }
+    }
+    // call function to run
+    playAnim();
+  }, 150)
+}
+
+//start animation
+playAnim();
+
+const formatDate = (date = new Date()) => {
+const day = date.getDate() + 1;
+const month = date.getMonth() + 1;
+const year = date.getFullYear();
+
+return `${month}/${day}/${year}`;
+}
+
+console.log(formatDate());
+
